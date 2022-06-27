db8950405ac272565f8e7604a4ced145dddfb234
savio-2-lopes <savioaugulopes@gmail.com>
2022-06-26 21:51:34 -0300

    :rocket: End lesson 1 and add apollo client

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,30 @@
+import { gql, useQuery } from "@apollo/client";
 
-function App() {
-  return (
-    <h1>Hello World</h1>
-  )
+const GET_LESSONS_QUERY = gql`
+  query {
+    lessons {
+      id
+      title
+    }
+  }
+`;
+
+interface Lesson {
+  id: string;
+  title: string
 }
 
-export default App
+function App() {
+  const { data } = useQuery<{lessons: Lesson[]}>(GET_LESSONS_QUERY);
+  console.log(data);
+
+  return (
+    <ul>
+      {data?.lessons.map((lesson) => {
+        return <li key={lesson.id}>{lesson.title}</li>;
+      })}
+    </ul>
+  );
+}
+
+export default App;
